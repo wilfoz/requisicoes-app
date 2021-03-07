fa06d549e8d5c32d74e703b5aae60d486b7645c5
wilfoz <wil_foz@yahoo.com.br>
2021-03-07 17:44:08 -0300

    update angular cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
+import { AuthguardService } from './services/authguard.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/public/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent }
+  { path: '', redirectTo: 'admin/painel', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin/painel', loadChildren: () => import('./components/admin/painel/painel.module').then(m => m.PainelModule), canActivate: [AuthguardService]},
+  { path: 'admin/funcionario', loadChildren: () => import('./components/admin/funcionario/funcionario.module').then(m => m.FuncionarioModule), canActivate: [AuthguardService]},
+  { path: 'admin/departamento', loadChildren: () => import('./components/admin/departamento/departamento.module').then(m => m.DepartamentoModule), canActivate: [AuthguardService]},
+  { path: 'admin/requisicao', loadChildren: () => import('./components/admin/requisicao/requisicao.module').then(m => m.RequisicaoModule), canActivate: [AuthguardService]},
+  { path: 'admin/movimentacao', loadChildren: () => import('./components/admin/movimentacao/movimentacao.module').then(m => m.MovimentacaoModule), canActivate: [AuthguardService]}
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { MenuComponent } from './components/admin/menu/menu.component';
+// Core Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Formulário
 import { FormsModule } from '@angular/forms';
 
+// Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-
-import { environment } from '../environments/environment';
+import { environment } from 'src/environments/environment';
+//Components sem Lazy
 import { LoginComponent } from './components/public/login/login.component';
-import { MenuComponent } from './components/admin/menu/menu.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -22,12 +28,13 @@ import { MenuComponent } from './components/admin/menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
